d3c5449f87ba560113387a922b11eec62b6a81e7
Steven Sidhu <steve.sidhu@hotmail.com>
2022-03-09 01:19:41 -0800

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+
+
+<title>Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+   <article>
+
+    <?= $post; ?>
+
+    </article>
+
+
+
+
+<a href="/">Back to homepage</a>
+
+</body>
